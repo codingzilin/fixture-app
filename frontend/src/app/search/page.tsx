36345972e401bffcd2531a8195ba370418e9b9cb
Zilin Song <harry.song518@gmail.com>
2025-05-05 14:56:18 +0930

    add search page

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import FixtureSearch from "../components/FixtureSearch";
+import Link from "next/link";
 
-export default function page() {
-  return <div>page</div>;
+export default function Searchpage() {
+  return (
+    <div className='max-w-4xl mx-auto px-4'>
+      <Link
+        href='/'
+        className='inline-block mb-6 text-blue-600 hover:text-blue-800'
+      >
+        Back to Upload
+      </Link>
+      <FixtureSearch />
+    </div>
+  );
 }
