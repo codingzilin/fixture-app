f3de771f105017a2688815069cab57718d98c6ea
Zilin Song <harry.song518@gmail.com>
2025-05-05 17:25:00 +0930

    update mongo uri

--- frontend/src/lib/mongodb.ts
+++ frontend/src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "";
+const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGODB || "";
 
 if (!MONGODB_URI) {
   throw new Error(
